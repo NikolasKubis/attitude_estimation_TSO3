44423697de692676be5ca3284d6be58066582d11
Nikolas Koumpis <nikolaos.p.koumpis@gmail.com>
2021-01-03 03:55:57 +0200

    adds UKF

--- UKF/sigma_points.m
+++ UKF/sigma_points.m
@@ -9,22 +9,11 @@ weight_m=zeros(2*L,1);
 
 kapa=0.01;
 
-alpha =0.9; % ayti einai i simantikiparametros. Apo edoelegxeis to pos tha katanemontai ta sigma points
-            % An i avevaiothta poy exeis einai mikri tote de xreiazetai na
-            % aploseis poli ta sigma points. An i avevaiothta einai megali
-            % prepei na ta aploseis polukai etsi to alpha prepei na to
-            % pareis megalo.
+alpha =0.9; 
 
 betta= 0.1;
 
-lambda=(alpha^2)*(L+kapa)-L; % prosexe oti to lambda pollaplasiazei ti stili tou covariance matrix
-                             % diladi tis kateythinsis avevaiotitas-kai
-                             % etsi ftiaxneis tin katanomi. An exeis pola
-                             % shmeia tote prepei na pareis magalyteres
-                             % apostaseis gia na einai katanemimena ta
-                             % s-points omoiomorfa. Etsi loipon to lambda
-                             % ayxanei me to dimension. Epeita, analoga me
-                             % tin avevaiotita poy exeis dialegeis to alpha
+lambda=(alpha^2)*(L+kapa)-L; 
                              
 
 weight_m(1)=lambda/(L+lambda);
